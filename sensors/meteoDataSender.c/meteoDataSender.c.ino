9eaa1bc962755a3cc89be8a3d387ee2e1af7cce7
mara747 <ma.el@seznam.cz>
2017-10-12 23:15:13 +0200

    Sending to server and deep sleep mode enabling

--- sensors/meteoDataSender.c/meteoDataSender.c.ino
+++ sensors/meteoDataSender.c/meteoDataSender.c.ino
@@ -6,6 +6,8 @@
 #include <Adafruit_BMP085.h>
 #include <Adafruit_SHT31.h>
 
+//#define LOG_DEBUG
+
 Adafruit_BMP085 sensorBMP180;
 Adafruit_SHT31 sensorSHT30;
 HTTPClient httpClient;
@@ -17,10 +19,12 @@ uint16_t altitudeBMP180 = 0;
 const char wifiSSID[] = "humbucak";
 const char wifiPass[] = "kareltoneni";
 
-const char temperatureID[] = "59dbd72017513c040894d21a";
-const char pressureID[] = "59dbd7d917513c040894d21b";
-const char humidityID[] = "59dbd81717513c040894d21c";
-const char altitudeID[] = "59dbd86517513c040894d21d";
+String temperatureID = "59dbd72017513c040894d21a";
+String pressureID = "59dbd7d917513c040894d21b";
+String humidityID = "59dbd81717513c040894d21c";
+String altitudeID = "59dbd86517513c040894d21d";
+
+template< typename T > void sendDataToServer(String aID, T aValue);
 
 void setup() {
   Serial.begin(115200);
@@ -42,30 +46,24 @@ void setup() {
     delay(500);
   }
 
+#ifdef LOG_DEBUG
   Serial << endl << "Connected. IP: " << WiFi.localIP() << endl;
+#endif 
 
-  httpClient.begin("13.95.93.26", 3000, String("/sensors/")+temperatureID);
+  sendDataToServer(temperatureID,temperatureSHT30);
+  sendDataToServer(pressureID,pressureBMP180);
+  sendDataToServer(humidityID,humiditySHT30);
+  sendDataToServer(altitudeID,altitudeBMP180);
 
-  Serial << "[HTTP] PUT..." << endl;
-  
-  int httpCode = httpClient.sendRequest("PUT","asd");
-  
-  if(httpCode > 0) {
-      Serial << "[HTTP] PUT... code: " + String(httpCode,2) << endl;
-
-      // file found at server
-      if(httpCode == HTTP_CODE_OK) {
-          String payload = httpClient.getString();
-          Serial << payload << endl;
-      }
-  } else {
-      Serial << "[HTTP] PUT... failed, error: " << httpClient.errorToString(httpCode) << endl;
-  }
-  httpClient.end();
+  int sleepSeconds = 60;
+  Serial << endl << "Going to sleep for "+String(sleepSeconds)+" seconds" << endl;
+  ESP.deepSleep(sleepSeconds * 1000000);
+  Serial << endl << "Weaking up!" << endl;
 }
 
 void loop() {
-
+  Serial << endl << "Entered the loop()" << endl;
+  while(1){};
 }
 
 void getValues() {
@@ -78,3 +76,34 @@ void getValues() {
   Serial << "BMP180 - temp:" << temperatureBMP180 << " press:" << pressureBMP180 << " alt:" << altitudeBMP180 << endl;
   Serial << "SHT30 - temp:" << temperatureSHT30 << " humidity:" << humiditySHT30 << endl;
 }
+
+
+template< typename T > void sendDataToServer(String aID, T aValue){
+  httpClient.begin("13.95.93.26", 3000, String("/sensors/")+aID);
+
+#ifdef LOG_DEBUG
+  Serial << "[HTTP] PUT..." << endl;
+#endif
+  String temperatureJSON = "{\"value\":"+String(aValue)+String("}");
+#ifdef LOG_DEBUG
+  Serial << "temperatureJSON=" << temperatureJSON << endl;
+#endif
+  httpClient.addHeader("Content-Type","application/json");
+  int httpCode = httpClient.sendRequest("PUT",temperatureJSON);
+  if(httpCode > 0) {
+#ifdef LOG_DEBUG
+      Serial << "[HTTP] PUT... code: " + String(httpCode,2) << endl;
+#endif      
+      // file found at server
+      if(httpCode == HTTP_CODE_OK) {
+          String payload = httpClient.getString();
+#ifdef LOG_DEBUG          
+          Serial << payload << endl;
+#endif          
+      }
+  } else {
+      Serial << "[HTTP] PUT... failed, error: " << httpClient.errorToString(httpCode) << endl;
+  }
+  httpClient.end();
+  }
+
