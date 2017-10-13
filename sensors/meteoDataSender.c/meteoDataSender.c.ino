7569f8ee1b8c66e1cdca3bffe3a22a1942ce875e
mara747 <ma.el@seznam.cz>
2017-10-13 23:08:36 +0200

    refactoring

--- sensors/meteoDataSender.c/meteoDataSender.c.ino
+++ sensors/meteoDataSender.c/meteoDataSender.c.ino
@@ -6,8 +6,6 @@
 #include <Adafruit_BMP085.h>
 #include <Adafruit_SHT31.h>
 
-//#define LOG_DEBUG
-
 Adafruit_BMP085 sensorBMP180;
 Adafruit_SHT31 sensorSHT30;
 HTTPClient httpClient;
@@ -19,17 +17,19 @@ uint16_t altitudeBMP180 = 0;
 const char wifiSSID[] = "humbucak";
 const char wifiPass[] = "kareltoneni";
 
-String temperatureID = "59dbd72017513c040894d21a";
-String pressureID = "59dbd7d917513c040894d21b";
-String humidityID = "59dbd81717513c040894d21c";
-String altitudeID = "59dbd86517513c040894d21d";
+String server = "104.40.218.233";
+
+String temperatureID = "59e124f1ed2d30079fd5f726";
+String pressureID = "59e12529ed2d30079fd5f727";
+String humidityID = "59e12560ed2d30079fd5f728";
+String altitudeID = "59e1259ded2d30079fd5f729";
 
 template< typename T > void sendDataToServer(String aID, T aValue);
 
 void setup() {
   Serial.begin(115200);
   Serial << endl << "meteo sender start" << endl;
-
+  
   if (!sensorBMP180.begin() || !sensorSHT30.begin(0x45)) {
     Serial << "Tlakomer nebo teplomer neodpovida. Zkontroluj zapojeni!" << endl;
     while (1) {}
@@ -43,12 +43,9 @@ void setup() {
   Serial << endl << "Wi-Fi connecting " << wifiSSID << " ";
   while (WiFi.status() != WL_CONNECTED) {
     Serial << ".";
+    Serial << WiFi.status() << endl;
     delay(500);
   }
-
-#ifdef LOG_DEBUG
-  Serial << endl << "Connected. IP: " << WiFi.localIP() << endl;
-#endif 
 
   sendDataToServer(temperatureID,temperatureSHT30);
   sendDataToServer(pressureID,pressureBMP180);
@@ -79,28 +76,16 @@ void getValues() {
 
 
 template< typename T > void sendDataToServer(String aID, T aValue){
-  httpClient.begin("13.95.93.26", 3000, String("/sensors/")+aID);
-
-#ifdef LOG_DEBUG
-  Serial << "[HTTP] PUT..." << endl;
-#endif
+  httpClient.begin(server, 3000, String("/sensors/")+aID);
   String temperatureJSON = "{\"value\":"+String(aValue)+String("}");
-#ifdef LOG_DEBUG
-  Serial << "temperatureJSON=" << temperatureJSON << endl;
-#endif
   httpClient.addHeader("Content-Type","application/json");
   int httpCode = httpClient.sendRequest("PUT",temperatureJSON);
   if(httpCode > 0) {
-#ifdef LOG_DEBUG
-      Serial << "[HTTP] PUT... code: " + String(httpCode,2) << endl;
-#endif      
-      // file found at server
-      if(httpCode == HTTP_CODE_OK) {
-          String payload = httpClient.getString();
-#ifdef LOG_DEBUG          
-          Serial << payload << endl;
-#endif          
-      }
+    Serial << "[HTTP] PUT... httpCode:" << httpCode << endl;
+    if(httpCode == HTTP_CODE_OK) {
+      String payload = httpClient.getString();
+      Serial << payload << endl;
+    }
   } else {
       Serial << "[HTTP] PUT... failed, error: " << httpClient.errorToString(httpCode) << endl;
   }
